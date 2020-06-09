9c1806c69979a7d1568321ff7709cfae566e821c
朋朋 <lpf.mart@gmail.com>
2020-06-09 17:47:28 +0800

    从json读取配置，并检测json文件的变动，来进行热更新

---
+++ ConfigureHotUpdate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace ConfigureHotUpdate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configurationRoot = builder.Build();
+
+            ChangeToken.OnChange(() => configurationRoot.GetReloadToken(), () =>
+            {
+                Console.WriteLine($"Key1: {configurationRoot["Key1"]}");
+            });
+
+            //IChangeToken token = configurationRoot.GetReloadToken();
+
+            //token.RegisterChangeCallback(state =>
+            //{
+            //}, configurationRoot);
+            Console.ReadKey();
+        }
+    }
+}
