d3c80b1d8299314582b4def72794e161d1d34a3b
朋朋 <lpf.mart@gmail.com>
2020-06-09 16:35:22 +0800

    .netcore DI 依赖注入

---
+++ DependencyInjectionDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjectionDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DependencyInjectionDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public int GetService(
+            [FromServices] IMySingletonService singletonService1,
+            [FromServices] IMySingletonService singletonService2
+            )
+        {
+            Console.WriteLine(singletonService1.GetHashCode());
+            Console.WriteLine(singletonService2.GetHashCode());
+            return 1;
+        }
+
+        [HttpGet]
+        public int GetServiceList([FromServices] IEnumerable<IOrderService> services)
+        {
+            foreach (var item in services)
+            {
+                Console.WriteLine($"获取到服务实例：{item}:{item.GetHashCode()}");
+            }
+            return 1;
+        }
+    }
+}
---
+++ DependencyInjectionDemo/Services/MySingletonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo.Services
+{
+    public interface IMySingletonService { }
+
+    public class MySingletonService : IMySingletonService
+    {
+    }
+
+    public class MySingletonServiceEx : IMySingletonService
+    {
+    }
+}
